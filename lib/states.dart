34049807cf89a203b414aac54f48b20206296bab
manar2002 <77124787+manar2002@users.noreply.github.com>
2022-10-14 00:02:31 +0200

    Delete states.dart

--- lib/states.dart
+++
@@ -1,5 +0,0 @@
-abstract class RegisterStates{}
-class RegisterInitState extends RegisterStates {}
-class RegisterLoadingState extends RegisterStates {}
-class RegisterSuccessState extends RegisterStates {}
-class RegisterErrorState extends RegisterStates {}
